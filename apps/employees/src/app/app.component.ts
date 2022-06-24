32f7ae7448fb08b37006a62b0acdd3cb9baa233e
Patricio Vargas <pato@onesignal.com>
2022-06-24 19:37:12 -0400

    employees rendering

--- apps/employees/src/app/app.component.ts
+++ apps/employees/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Employee {
+  name: string;
+  title: string;
+}
+
 @Component({
   selector: 'onesignal-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employees';
+  employees: Employee[] = [
+    { 
+      name: 'John Doe',
+      title: 'Todo 1'
+     },
+    { 
+      name: 'Jane Doe',
+      title: 'Todo 2'
+     }
+  ];
+
+  addTodo() {
+    this.employees.push({
+      name: `John Doe ${Math.floor(Math.random() * 1000)}`,
+      title: `Title ${Math.floor(Math.random() * 1000)}`,
+    });
+  }
 }
